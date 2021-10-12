b1616b31ebb9543d8a93478cf2453f54f50d34cf
DevXO <dsemien2@gmail.com>
2021-10-11 20:08:29 -0700

    Added tests for classes

--- SchoolCatalogue/script.js
+++ SchoolCatalogue/script.js
@@ -32,7 +32,7 @@ class School {
   }
 
   static pickSubstituteTeacher(substituteTeachers) {
-    let ranNum = Math.floor(Math.random() * this.substituteTeachers.length);
+    let ranNum = Math.floor(Math.random() * substituteTeachers.length);
     return substituteTeachers[ranNum];
   }
 }
@@ -62,3 +62,29 @@ class HighSchool extends School {
     return this._sportsTeams;
   }
 }
+
+const lorraineHansbury = new PrimarySchool(
+  'Lorraine Hansbury',
+  514,
+  'Students must be picked up by a parent, guardian, or a family member over the age of 13.'
+);
+
+lorraineHansbury.quickFacts();
+console.log(
+  School.pickSubstituteTeacher([
+    'Jamal Crawford',
+    'Lou Williams',
+    'J. R. Smith',
+    'James Harden',
+    'Jason Terry',
+    'Manu Ginobli',
+  ])
+);
+
+const alSmith = new HighSchool('Al E. Smith', 415, [
+  'Baseball',
+  'Basketball',
+  'Volleyball',
+  'Track and Field',
+]);
+console.log(alSmith.sportsTeams);
